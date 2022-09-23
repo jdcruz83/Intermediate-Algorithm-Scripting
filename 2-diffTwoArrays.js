bbabd86630cce90dcf4fbe5583ba9ce854c94c02
Jorge Cruz <100860547+jdcruz83@users.noreply.github.com>
2022-09-23 19:57:34 +0100

    file updates

--- 2-diffTwoArrays.js
+++ 2-diffTwoArrays.js
@@ -2,9 +2,15 @@
 Note: You can return the array with its elements in any order.*/
 
 function diffArray(arr1, arr2) {
-    const newArr = [];
+    let newArr = [];
+    let newArr1 = arr1.filter(a => !arr2.includes(a))
+    let newArr2 = arr2.filter(a => !arr1.includes(a));
+    newArr = newArr1.concat(newArr2);
     return newArr;
 }
 
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+console.log(diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]));
+console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
+console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
