5fc4e183822533b1d15281fffb5a024ee984c27f
Jorge Cruz <100860547+jdcruz83@users.noreply.github.com>
2022-09-25 02:51:30 +0100

    Update 9-missingLetters.js

--- 9-missingLetters.js
+++ 9-missingLetters.js
@@ -2,8 +2,20 @@
 //If all letters are present in the range, return undefined.
 
 function fearNotLetter(str) {
+    //Var
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    
+    //Case there's no missing letter;
+    if (str.length === alphabet.length) {
+      return undefined;
+    }
   
     return str;
   }
   
-  fearNotLetter("abce");
+  //fearNotLetter("abce");
+  console.log(fearNotLetter("abce"));
+  console.log(fearNotLetter("abcdefghjklmno"));
+  console.log(fearNotLetter("stvwx"));
+  console.log(fearNotLetter("bcdf"));
+  console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz"));
