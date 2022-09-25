082a39994886c42e3dd803c4ba8cdaca771bea24
Jorge Cruz <100860547+jdcruz83@users.noreply.github.com>
2022-09-25 23:01:03 +0100

    Update 9-missingLetters.js

--- 9-missingLetters.js
+++ 9-missingLetters.js
@@ -2,20 +2,43 @@
 //If all letters are present in the range, return undefined.
 
 function fearNotLetter(str) {
-    //Var
-    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  //Var
+  let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  str = str.split(""); // turn string to array
+  
+  //Case there's no missing letter;
+  if (str.length === alphabet.length) {
+    return undefined;
+  } else {
+
+    //Get initial and final letters as they are provided in order
+    let firstLetter = str[0]; // ok
+    let lastLetter = str[str.length - 1]; // ok
+
+    //search alphabet for first and last letter indexes
+    let firstPos = alphabet.indexOf(firstLetter); // ok
+    let lastPos = alphabet.indexOf(lastLetter); // ok
+
+    //retrive the complete string from initial to final / string to array
+    let alphaArr = alphabet.slice(firstPos, lastPos + 1);
     
-    //Case there's no missing letter;
-    if (str.length === alphabet.length) {
-      return undefined;
-    }
-  
-    return str;
+    //filter strings for letter missing; // turn to string
+    let myArr = alphaArr.filter(i => !str.includes(i)).join();
+    str = myArr;
+
+    //debug...
+    //console.log(alphabet);
+    //console.log(firstPos, firstLetter);
+    //console.log(lastPos, lastLetter);
+    //console.log(alphaArr);
+    //console.log(myArr);
   }
-  
-  //fearNotLetter("abce");
-  console.log(fearNotLetter("abce"));
-  console.log(fearNotLetter("abcdefghjklmno"));
-  console.log(fearNotLetter("stvwx"));
-  console.log(fearNotLetter("bcdf"));
-  console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz"));
+  return str;
+}
+
+fearNotLetter("abce");
+console.log(fearNotLetter("abce"));
+console.log(fearNotLetter("abcdefghjklmno"));
+console.log(fearNotLetter("stvwx"));
+console.log(fearNotLetter("bcdf"));
+console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz"));
