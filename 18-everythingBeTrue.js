96de84b94cdb25cf387327dbb1fa36ea0037da49
Jorge Cruz <100860547+jdcruz83@users.noreply.github.com>
2022-09-29 11:53:41 +0100

    Update 18-everythingBeTrue.js

--- 18-everythingBeTrue.js
+++ 18-everythingBeTrue.js
@@ -4,32 +4,13 @@
 //Remember, you can access object properties through either dot notation or [] notation.
 
 function truthCheck(collection, pre) {
-    let obj = Object.values(collection);
-    let result = "";
-
-    for (let i in collection) {
-        if (obj[i].hasOwnProperty(pre) === true) {
-            return true;
-        }
-        return false;
-    }
-    console.log(obj);
-    //return pre;
+    return collection.every(item => item[pre]);
 }
 
-truthCheck([{ 
-    name: "Quincy", 
-    role: "Founder", 
-    isBot: false 
-    }, 
-    { 
-    name: "Naomi", 
-    role: "", 
-    isBot: false 
-    },
-    { 
-    name: "Camperbot", 
-    role: "Bot", 
-    isBot: true }], 
-    "isBot");
+truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot");
+truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "name");
+truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "role")
+
 console.log(truthCheck([{ name: "Quincy", role: "Founder", isBot: false }, { name: "Naomi", role: "", isBot: false }, { name: "Camperbot", role: "Bot", isBot: true }], "isBot"));
+console.log(truthCheck([{ name: "Quincy", role: "Founder", isBot: false }, { name: "Naomi", role: "", isBot: false }, { name: "Camperbot", role: "Bot", isBot: true }], "name"));
+console.log(truthCheck([{ name: "Quincy", role: "Founder", isBot: false }, { name: "Naomi", role: "", isBot: false }, { name: "Camperbot", role: "Bot", isBot: true }], "role"));
